a93dbd3e8253161f75f1ec407d31897d4e0c1f8a
Ilya Zdesenko <izdesenko@work.local>
2017-01-12 17:01:11 +0300

    Добавил сингл-файл решения для php аналога js-скрипта.

--- index.php
+++ index.php
@@ -86,4 +86,29 @@ class Coder{
 	}
 	 */
 }
+
+$source = [
+	'red/apple/candy/jujube/red','green/apple/candy','red/garnet/juice','green/grapes/candy',
+	'orange/orange/coctail/alcohol','green/apple/jujube/green','green/grapes/ice cream','red/apple/candy',
+	'orange/orange/juice','orange/orange/pie','red/strawberry/candy','red/strawberry/pie',
+	'orange/orange/coctail/non alcohol','red/apple/puree','green/grapes/juice','green/grass/silo',
+	'red/strawberry/juice','red/apple/candy/jujube/green','red/garnet/candy/red','red/grapes/ice cream',
+	'red/garnet/candy/jujube','red/apple/candy/jujube/blue','green/apple/jujube/green','red/strawberry/ice cream',
+	'red/apple/ice cream','red/garnet/pie','green/grass/hay','green/lime/coctail/non alcohol','red/grapes/juice',
+	'green/lime/coctail/alcohol','green/lime/tea','green/apple/puree','green/lime/mousse','red/apple/candy/sweet',
+	'green/apple/sweet','red/grapes/candy'];
+$passphrase = '8c095ceb1f3b84e4943b4fd526c5461c';
+
+$c = new Coder($source,$passphrase);
+$packed = $c->pack($source);
+$encrypted = $c->encrypt($packed);
+$decrypted = $c->decrypt($encrypted);
+$unpacked = $c->unpack($decrypted);
+
+echo 'Packed:::: '.$packed."\n";
+echo 'Decrypted: '.$decrypted."\n";
+echo 'Encrypted: '.$encrypted."\n\n";
+echo 'Unpacked:: '.json_encode($unpacked)."\n\n";
+echo 'Source:::: '.json_encode($source)."\n\n";
+
 ?>
--- lib/conf.php
+++ lib/conf.php
@@ -1,5 +1,5 @@
 <?php
-$reference = [
+$source = [
 	'red/apple/candy/jujube/red','green/apple/candy','red/garnet/juice','green/grapes/candy',
 	'orange/orange/coctail/alcohol','green/apple/jujube/green','green/grapes/ice cream','red/apple/candy',
 	'orange/orange/juice','orange/orange/pie','red/strawberry/candy','red/strawberry/pie',
@@ -10,5 +10,4 @@ $reference = [
 	'green/lime/coctail/alcohol','green/lime/tea','green/apple/puree','green/lime/mousse','red/apple/candy/sweet',
 	'green/apple/sweet','red/grapes/candy'];
 $passphrase = '8c095ceb1f3b84e4943b4fd526c5461c';
-$iv = 'f53aa4c83f40939b8873a4760a3c3bb2';
 ?>
